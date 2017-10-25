56d8f026d5e559a5e396fdb3047dc77d64a4d9e7
peter.ji <peter.ji@rccchina.com>
2017-10-25 17:36:32 +0800

    前段项目

--- app/serializers/card_list_serializer.rb
+++ app/serializers/card_list_serializer.rb
@@ -1,5 +1,5 @@
 class CardListSerializer < ActiveModel::Serializer
-  attributes :id, :raw_name, :name, :card_code
+  attributes :id, :raw_name, :name, :card_code, :color, :level, :cost, :attack_value, :soul
 
   belongs_to :product
   belongs_to :new_product
