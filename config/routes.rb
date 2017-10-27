002f77359d1de0e84dbeb8adc4d54740d92ebd59
peter.ji <peter.ji@rccchina.com>
2017-10-27 13:11:04 +0800

    路由忘记提交了

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cards
+      resources :products
+      resources :new_products
+      resources :triggers
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
